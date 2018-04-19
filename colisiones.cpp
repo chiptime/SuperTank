86ecc23d0ed20b4c5e90c5f5c750e636315e44c5
guiloor <guille-497@hotmail.com>
2018-04-19 11:33:16 +0200

    Update colisiones.cpp

--- colisiones.cpp
+++ colisiones.cpp
@@ -1,23 +1,49 @@
-function shoot (ob) {
-    ob.lastshot=getTimer();
-    game.bulletcounter++;
-    if (game.bulletcounter>100) {
-        game.bulletcounter=0;
-    }
-    var name = "bullet"+game.bulletcounter;
-    game[name]= new game.Bullet;
-    game[name].id=game.bulletcounter;
-    game.bullets.push(game[name]);
-    if (ob.dirx or ob.diry) {
-        game[name].dirx= ob.dirx;
-        game[name].diry= ob.diry;
-    }
-    game[name].xtile= ob.xtile;
-    game[name].ytile= ob.ytile;
-    game.clip.attachMovie("bullet", name, 10100+game.bulletcounter);
-    game[name].clip=game.clip[name];
-    game[name].x = (ob.x+game[name].dirx*ob.width);
-    game[name].y = (ob.y+game[name].diry*ob.height);
-    game.clip[name]._x = game[name].x;
-    game.clip[name]._y = game[name].y;
+
+
+int xt = 25,yt = 11;//cordenadas de tanque
+int xb = 0,yb = 0;//cordenadas de bala de tanque
+int xn = 5,yn = 18;//cordenadas de enemigo1
+
+void checabala(){
+	if (xb == xn && yb == yn+1 || xb == xn+2 && yb == yn+1 || xb == xn+3 && yb == yn+1 || xb == xn+4 && yb == yn+1 || xb+1 == xn+3 && yb == yn+2 || xb+1 == xn+3 && yb == yn){
+		tanques ++;	
+		if (yn > 11 && yt > 11){
+			gotoxy(xn,yn); printf   ("     ");
+			gotoxy(xn,yn+1); printf ("     ");
+			gotoxy(xn,yn+2); printf ("     ");
+			xn = 5;
+			yn = 5;
+			xb = 0;
+			yb = 0;
+			gotoxy(xn,yn); printf   (" ***");
+			gotoxy(xn,yn+1); printf ("  *** ");
+			gotoxy(xn,yn+2); printf (" ***");	
+		}
+			else{
+				if(yn < 11 && yt < 11){
+					gotoxy(xn,yn); printf   ("     ");
+					gotoxy(xn,yn+1); printf ("     ");
+					gotoxy(xn,yn+2); printf ("     ");
+					xn = 5;
+					yn = 18;
+					xb = 0;
+					yb = 0;
+					gotoxy(xn,yn); printf   (" ***");
+					gotoxy(xn,yn+1); printf ("  *** ");
+					gotoxy(xn,yn+2); printf (" ***");	
+				}
+	
+			}
+	}
+	
 }
+	if (xbn == xbn2 && ybn-1 == ybn2 || xbn == xbn2 && ybn+1 == ybn2 || xbn == xbn2 && ybn == ybn2){
+		gotoxy(xbn,ybn);printf (" ");
+		xbn = 0;
+		ybn = 0;
+		xbn2 = 0;
+		ybn2 = 0;
+	}
+}
+
+  
